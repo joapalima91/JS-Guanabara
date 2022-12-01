3f04e0d1b5928099f8413ce6f49fac8af2e38f6e
João Paulo da Silva Lima <joaopalima91@gmail.com>
2022-12-01 12:35:09 -0300

    criado primeiro codigo com array

---
+++ Modulo F/array1.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 2, 9, 3]
+// num[3] = 5 adiciona item na última posição
+num.push(1) // adiciona número na ultima posição automaticamente.
+num.sort() // ordena do menor pro maior
+console.log(`Nosso vetor é o ${num}`) //imprime vetor em forma crescente
+//console.log(num.length) // imprime a quantidade de elementos no vetor
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
