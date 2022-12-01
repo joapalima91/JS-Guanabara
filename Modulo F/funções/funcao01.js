341e5a303305570ff934a4ee71c2bb0b725016e4
João Paulo da Silva Lima <joaopalima91@gmail.com>
2022-12-01 15:33:36 -0300

    efetuado a funcao parimpar

---
+++ Modulo F/funções/funcao01.js
@@ -0,0 +1,12 @@
+function parimpar (n) {
+    if (n%2==0){
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+
+console.log(parimpar(122))
+console.log(parimpar(123))
+console.log(parimpar(124))
+console.log(parimpar(125))
